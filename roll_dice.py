d0cde2a728a671bec07a93c9a22047f55f97f86b
Gabriel Fontenelle <31320673+ngsylar@users.noreply.github.com>
2021-03-12 02:13:28 -0300

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -2,29 +2,35 @@ import random
 
 def roll_dice(msg):
 
-  # interpretar dados
+  # interpretar dados (valores iniciais)
   dices = []
+  multi = 0
+  dicesum = 0
+
+  # interpretar dados (numero de dados)
   raw1 = msg.casefold().split('sd', 1)
-  scope = int(raw1[0])
-  if (scope < 1) or (scope > 100):
+  amount = int(raw1[0])
+  if (amount < 1) or (amount > 100):
     raise ValueError
+
+  # interpretar dados (valor maximo do dado)
   raw2 = raw1[1].replace('-','+-').split('+',1)
   dicelen = int(raw2[0])
   if (dicelen < 2) or (dicelen > 200):
     raise ValueError
 
+  # interpretar dados (somas)
+  if len(raw2) > 1:
+    if raw2[1][-1].casefold() == 'e':
+      multi = 1
+      raw2[1] = raw2[1][:-1]
+    d_sums = raw2[1].split('+')
+    for d_sum in d_sums:
+      dicesum += int(d_sum)
+
   # lancar dados
-  multi = 0
-  for i in range(scope):
+  for i in range(amount):
     value = random.randint(1, dicelen)
-    if len(raw2) > 1:
-      if raw2[1][-1].casefold() == 'e':
-        multi = 1
-        raw2[1] = raw2[1][:-1]
-      dicesum = 0
-      d_sums = raw2[1].split('+')
-      for d_sum in d_sums:
-        dicesum += int(d_sum)
     dices.append(value)
 
   # transcrever dados
@@ -37,17 +43,11 @@ def roll_dice(msg):
   for dice in dices:
 
     # guardar valores
-    if (len(raw2) > 1) and multi:
-      # soma em cada dado (ataque)
-      value = dice + dicesum
-      values.append(value)
-    else:
-      # soma final (dano) ou sem soma
-      value = dice
-      values.append(value)
+    value = dice + dicesum
+    values.append(value)
     valsum += value
 
-    # verificar valor min e maximo
+    # verificar menor valor alcancado
     dice_i += 1
     if value < dmin[1]:
       dmin = dmin[:2]
@@ -57,6 +57,8 @@ def roll_dice(msg):
         dmin[0] = 1
     elif value == dmin[1]:
       dmin.append(dice_i)
+
+    # verificar maior valor alcancado
     if value > dmax[1]:
       dmax = dmax[:2]
       dmax[1] = value
@@ -82,7 +84,7 @@ def roll_dice(msg):
   # mostrar resultado
   dmin_type = ['Lowest', '**Critical Failure**']
   dmax_type = ['Highest', '**Critical Strike**']
-  if scope > 1:
+  if amount > 1:
     sum_desc = sum_desc+'\n\u0060 '+str(dmin[1])+' \u0060 \u27F5 '+dmin_type[dmin[0]]+' ('+str(dmin[2:])[1:-1]+')d\n\u0060 '+str(dmax[1])+' \u0060 \u27F5 '+dmax_type[dmax[0]]+' ('+str(dmax[2:])[1:-1]+')d'
   sendmsg = '\u0060 '+str(valsum)+' \u0060'+' \u27F5 '+str(values)+' '+d_name+sum_desc
   return sendmsg
