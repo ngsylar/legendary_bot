a4fd66247dc71f2d4deb6bc12a1c1f3b8143d419
ngsylar <ngsylar@gmail.com>
2021-04-17 13:43:13 -0300

    Cleaning main

--- main.py
+++ main.py
@@ -5,79 +5,76 @@ from roll_dice import random, Dice
 from guild_db import db, GuildDB
 from keep_alive import keep_alive
 
-client = discord.Client()
+bot = discord.Client()
+guild = GuildDB()
+dice = Dice()
 
-@client.event
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-  await client.change_presence(activity=discord.Game(name='RPG'))
+  print('We have logged in as {0.user}'.format(bot))
+  await bot.change_presence(activity=discord.Game(name='RPG'))
 
-@client.event
+@bot.event
 async def on_message(message):
-  msgAuthor = message.author.guild_permissions
-  msg = message.content
-  server = GuildDB()
-
-  if message.author == client.user:
+  if message.author == bot.user:
     return
 
   # ---------------------------------------------------
   # respostas padrao
 
-  if str(client.user.id) in msg:
-    await message.channel.send(server.whoami)
+  if str(bot.user.id) in message.content:
+    await message.channel.send(guild.whoami)
   
-  if msg.startswith('legen!dary'):
+  if message.content.startswith('legen!dary'):
     option = random.randint(0, 7)
-    await message.channel.send('<@'+str(message.author.id)+'>, '+server.legendary[option])
+    await message.channel.send('<@'+str(message.author.id)+'>, '+guild.legendary[option])
 
 
   # ---------------------------------------------------
   # definicao de canal secreto
   
-  server_id = str(message.guild.id)
+  guild_id = str(message.guild.id)
 
-  if msg.startswith('legen!ch'):
-    if msgAuthor.manage_channels:
-      server.update_sch(server_id, msg)
-      await message.channel.send(server.operationStatus)
+  if message.content.startswith('legen!ch'):
+    if message.author.guild_permissions.manage_channels:
+      guild.update_sch(guild_id, message.content)
+      await message.channel.send(guild.operationStatus)
     else:
-      await message.channel.send('<@'+str(message.author.id)+'>, '+server.sorry)
+      await message.channel.send('<@'+str(message.author.id)+'>, '+guild.sorry)
   
-  if msg.startswith('legen!del'):
-    if msgAuthor.manage_channels:
-      if server_id in db.keys():
-        server.delete_sch(server_id)
-        await message.channel.send(server.operationStatus)
+  if message.content.startswith('legen!del'):
+    if message.author.guild_permissions.manage_channels:
+      if guild_id in db.keys():
+        guild.delete_sch(guild_id)
+        await message.channel.send(guild.operationStatus)
     else:
-      await message.channel.send('<@'+str(message.author.id)+'>, '+server.sorry)
+      await message.channel.send('<@'+str(message.author.id)+'>, '+guild.sorry)
   
-  if msg.startswith('play!book'):
-    if msgAuthor.manage_channels:
-      if server_id in db.keys():
-        await message.channel.send('<#'+db[server_id]+'> \u27F5 The Secret Channel')
+  if message.content.startswith('play!book'):
+    if message.author.guild_permissions.manage_channels:
+      if guild_id in db.keys():
+        await message.channel.send('<#'+db[guild_id]+'> \u27F5 The Secret Channel')
       else:
-        await message.channel.send(server.sometimes)
+        await message.channel.send(guild.sometimes)
     else:
-      await message.channel.send('<@'+str(message.author.id)+'>, '+server.sorry)
+      await message.channel.send('<@'+str(message.author.id)+'>, '+guild.sorry)
 
 
   # ---------------------------------------------------
   # usar canal secreto
 
-  if re.match('[0-9]+[Ss]?[Dd][0-9]+', msg):
-    dice = Dice()
+  if re.match('[0-9]+[Ss]?[Dd][0-9]+', message.content):
     try:
-      if re.match('[0-9]+[Ss]', msg) and (server_id in db.keys()):
-        secretChannel = client.get_channel(int(db[server_id]))
-        diceName = msg.lower().replace('sd','d')
+      if re.match('[0-9]+[Ss]', message.content) and (guild_id in db.keys()):
+        secretChannel = bot.get_channel(int(db[guild_id]))
+        diceName = message.content.lower().replace('sd','d')
         rollResults = dice.roll(diceName)
-        await message.channel.send('<@'+str(message.author.id)+'>, '+ server.awesome)
+        await message.channel.send('<@'+str(message.author.id)+'>, '+ guild.awesome)
         await secretChannel.send('<@'+str(message.author.id)+'>,\n' + rollResults)
       else:
-        diceName = msg.lower()
+        diceName = message.content.lower()
         rollResults = dice.roll(diceName)
-        await message.channel.send('<@'+str(message.author.id)+'>, '+ server.awesome + '\n' + rollResults)
+        await message.channel.send('<@'+str(message.author.id)+'>, '+ guild.awesome + '\n' + rollResults)
     except ValueError:
       return
 
@@ -85,4 +82,4 @@ async def on_message(message):
   # ---------------------------------------------------
 
 keep_alive()
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
