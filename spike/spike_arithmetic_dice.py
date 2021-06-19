cbc34f2554e103fd8288a6915d2f7119772efeb0
ngsylar <ngsylar@gmail.com>
2021-06-19 13:49:53 -0300

    Spikes cleaning

--- spike/spike_arithmetic_dice.py
+++ spike/spike_arithmetic_dice.py
@@ -1,43 +1,9 @@
-# (?:(?!ab).)*
 import re
-
-roll = '-13-9+1d20+684-687e+684+2d8+7-3e+4e+1'
-
-rolls = []
-
-diceRegex = r'\d+[SsHh]?[Dd]\d+'
-sumRegex = r'(?:(?![+\-]?\d+d).)*'
-
-rolls.append(re.match(sumRegex, roll)[0])
-rolls.extend(re.findall(r'[+\-]?\d+d'+sumRegex, roll))
-rolls = list(filter(None, rolls))
-
-print(rolls)
-
-#-------------------------------------------------------------------
-
-def rolardados():
-    return 1
+from spike_modules import Regex as rx, rolardados
 
 expressionInput = '(((1D8+4/2+100*3,22+2D20-1each+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
 #expressionInput = '(((4/2+100*3,22+2D20-1e+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
 #expressionInput = '(((4/2+10*-3.2-2)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
-
-diceRegex = r'([\*\/\+\-])?(\d+d\d+)'
-#modifierRegex = r'([\*\/\+\-])(\d+(?:\.\d+)?)e'
-modifiersContextRegex = r'(?:(?![\*\/\+\-]\-?\d+d).)*'
-floatValueRegex = '(\-?\d+(?:\.\d+)?)'
-
-arithContextRegex = r'(?:\()([^()]+)(?:\))'
-arithMulRegex = floatValueRegex + r'\*' + floatValueRegex
-arithDivRegex = floatValueRegex + r'\/' + floatValueRegex
-arithAddRegex = floatValueRegex + r'\+' + floatValueRegex
-arithSubRegex = floatValueRegex + r'\-' + floatValueRegex
-
-modifierMulRegex = r'\*'+ floatValueRegex +r'e'
-modifierDivRegex = r'\/'+ floatValueRegex +r'e'
-modifierAddRegex = r'\+'+ floatValueRegex +r'e'
-modifierSubRegex = r'\-'+ floatValueRegex +r'e'
 
 def compute_arith (context, operation, result):
     context = context[:operation.start()] + str(result) + context[operation.end():]
@@ -45,54 +11,54 @@ def compute_arith (context, operation, result):
 
 expressionRaw = '('+expressionInput.lower().replace('each', 'e').replace(',', '.')+')'
 print(expressionRaw)
-matchedArith = re.search(arithContextRegex, expressionRaw)
-arithContext = matchedArith[1]
+matchedArithContext = re.search(rx.arithContext, expressionRaw)
+arithContext = matchedArithContext[1]
 print(arithContext)
 
-matched_d = re.search(diceRegex, arithContext)
+matched_d = re.search(rx.dice, arithContext)
 while matched_d:
-    modifiersMarker = {}
-    modifiersMarker['start'] = matched_d.end()
-    matchedModifiers = re.match(modifiersContextRegex, arithContext[modifiersMarker['start']:])
-    modifiersMarker['end'] = matched_d.end() + matchedModifiers.end()
-    modifiersRaw = matchedModifiers[0]
+    modifiersContextMarker = {}
+    modifiersContextMarker['start'] = matched_d.end()
+    matchedModifiersContext = re.match(rx.modifiersContext, arithContext[modifiersContextMarker['start']:])
+    modifiersContextMarker['end'] = matched_d.end() + matchedModifiersContext.end()
+    modifiersContext = matchedModifiersContext[0]
 
     # editar: aqui usar o lancamento de dado (neste trecho de codigo foi usado apenas uma substituicao simples, pois o lancamento ja foi implementado, nao sendo o foco deste teste)
     resultados = []
     resultados_simples = rolardados()
     resultados_compostos = resultados_simples
     while 1:
-        mod_is_mul = re.search(modifierMulRegex, modifiersRaw)
-        mod_is_div = re.search(modifierDivRegex, modifiersRaw)
-        mod_is_add = re.search(modifierAddRegex, modifiersRaw)
-        mod_is_sub = re.search(modifierSubRegex, modifiersRaw)
+        mod_is_mul = re.search(rx.modifierMul, modifiersContext)
+        mod_is_div = re.search(rx.modifierDiv, modifiersContext)
+        mod_is_add = re.search(rx.modifierAdd, modifiersContext)
+        mod_is_sub = re.search(rx.modifierSub, modifiersContext)
         
         if mod_is_mul:
             resultados_compostos *= float(mod_is_mul[1])
-            modifiersRaw = compute_arith(modifiersRaw, mod_is_mul, '')
+            modifiersContext = compute_arith(modifiersContext, mod_is_mul, '')
         elif mod_is_div:
             resultados_compostos /= float(mod_is_div[1])
-            modifiersRaw = compute_arith(modifiersRaw, mod_is_div, '')
+            modifiersContext = compute_arith(modifiersContext, mod_is_div, '')
         elif mod_is_add:
             resultados_compostos += float(mod_is_add[1])
-            modifiersRaw = compute_arith(modifiersRaw, mod_is_add, '')
+            modifiersContext = compute_arith(modifiersContext, mod_is_add, '')
         elif mod_is_sub:
             resultados_compostos -= float(mod_is_sub[1])
-            modifiersRaw = compute_arith(modifiersRaw, mod_is_sub, '')
+            modifiersContext = compute_arith(modifiersContext, mod_is_sub, '')
         else:
             resultado_total_composto = resultados_compostos
-            arithContext = arithContext[:modifiersMarker['start']] + modifiersRaw + arithContext[modifiersMarker['end']:]
+            arithContext = arithContext[:modifiersContextMarker['start']] + modifiersContext + arithContext[modifiersContextMarker['end']:]
             arithContext = arithContext[:matched_d.start()] + (matched_d[1] or '') + str(resultado_total_composto) + arithContext[matched_d.end():]
             print(arithContext)
             break
 
-    matched_d = re.search(diceRegex, arithContext)
+    matched_d = re.search(rx.dice, arithContext)
 
 while 1:
-    op_is_mul = re.search(arithMulRegex, arithContext)
-    op_is_div = re.search(arithDivRegex, arithContext)
-    op_is_add = re.search(arithAddRegex, arithContext)
-    op_is_sub = re.search(arithSubRegex, arithContext)
+    op_is_mul = re.search(rx.arithMul, arithContext)
+    op_is_div = re.search(rx.arithDiv, arithContext)
+    op_is_add = re.search(rx.arithAdd, arithContext)
+    op_is_sub = re.search(rx.arithSub, arithContext)
     
     if op_is_mul:
         mulResult = float(op_is_mul[1]) * float(op_is_mul[2])
---
+++ spike/spike_modules.py
@@ -0,0 +1,19 @@
+class Regex:
+    dice = r'([\*\/\+\-])?(\d+d\d+)'
+    #modifier = r'([\*\/\+\-])(\d+(?:\.\d+)?)e'
+    modifiersContext = r'(?:(?![\*\/\+\-]\-?\d+d).)*'
+    floatValue = '(\-?\d+(?:\.\d+)?)'
+
+    arithContext = r'(?:\()([^()]+)(?:\))'
+    arithMul = floatValue + r'\*' + floatValue
+    arithDiv = floatValue + r'\/' + floatValue
+    arithAdd = floatValue + r'\+' + floatValue
+    arithSub = floatValue + r'\-' + floatValue
+
+    modifierMul = r'\*'+ floatValue +r'e'
+    modifierDiv = r'\/'+ floatValue +r'e'
+    modifierAdd = r'\+'+ floatValue +r'e'
+    modifierSub = r'\-'+ floatValue +r'e'
+
+def rolardados():
+    return 1
