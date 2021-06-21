d4765266949e311381672875b07864404c679700
ngsylar <ngsylar@gmail.com>
2021-06-20 22:46:17 -0300

    Spike: new classes for cleaning arithmetic dice

--- spike/spike_arithmetic_dice.py
+++ spike/spike_arithmetic_dice.py
@@ -1,5 +1,5 @@
 import re
-from spike_modules import DefaultRegexes as rx, rolardados
+from spike_modules import DefaultRegexes as regex, Arithmetic, DiceEffect, rolardados
 
 expressionInput = '(((1D8+4/2+100*3,22+2D20-1each+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
 #expressionInput = '(((4/2+100*3,22+2D20-1e+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
@@ -12,16 +12,16 @@ def compute_arith (context, operation, result):
 expressionRaw = '('+expressionInput.lower().replace('each', 'e').replace(',', '.')+')'
 print(expressionRaw)
 
-matchedArithContext = re.search(rx.arithContext, expressionRaw)
+matchedArithContext = re.search(regex.ARITH_CONTEXT, expressionRaw)
 while matchedArithContext:
     arithContext = matchedArithContext[1]
     # print(arithContext)
 
-    matched_d = re.search(rx.dice, arithContext)
+    matched_d = re.search(regex.DICE, arithContext)
     while matched_d:
         modifiersContextMarker = {}
         modifiersContextMarker['start'] = matched_d.end()
-        matchedModifiersContext = re.match(rx.modifiersContext, arithContext[modifiersContextMarker['start']:])
+        matchedModifiersContext = re.match(regex.MODIFIERS_CONTEXT, arithContext[modifiersContextMarker['start']:])
         modifiersContextMarker['end'] = matched_d.end() + matchedModifiersContext.end()
         modifiersContext = matchedModifiersContext[0]
 
@@ -29,24 +29,21 @@ while matchedArithContext:
         resultados = []
         resultados_simples = rolardados()
         resultados_compostos = resultados_simples
+        
+        current = DiceEffect()
         while 1:
-            mod_is_mul = re.search(rx.modifierMul, modifiersContext)
-            mod_is_div = re.search(rx.modifierDiv, modifiersContext)
-            mod_is_add = re.search(rx.modifierAdd, modifiersContext)
-            mod_is_sub = re.search(rx.modifierSub, modifiersContext)
-            
-            if mod_is_mul:
-                resultados_compostos *= float(mod_is_mul[1])
-                modifiersContext = compute_arith(modifiersContext, mod_is_mul, '')
-            elif mod_is_div:
-                resultados_compostos /= float(mod_is_div[1])
-                modifiersContext = compute_arith(modifiersContext, mod_is_div, '')
-            elif mod_is_add:
-                resultados_compostos += float(mod_is_add[1])
-                modifiersContext = compute_arith(modifiersContext, mod_is_add, '')
-            elif mod_is_sub:
-                resultados_compostos -= float(mod_is_sub[1])
-                modifiersContext = compute_arith(modifiersContext, mod_is_sub, '')
+            if current.modifier_operator_is_mul(modifiersContext):
+                resultados_compostos *= current.modifier_value
+                modifiersContext = compute_arith(modifiersContext, current.modifier_effect, '')
+            elif current.modifier_operator_is_div(modifiersContext):
+                resultados_compostos /= current.modifier_value
+                modifiersContext = compute_arith(modifiersContext, current.modifier_effect, '')
+            elif current.modifier_operator_is_add(modifiersContext):
+                resultados_compostos += current.modifier_value
+                modifiersContext = compute_arith(modifiersContext, current.modifier_effect, '')
+            elif current.modifier_operator_is_sub(modifiersContext):
+                resultados_compostos -= current.modifier_value
+                modifiersContext = compute_arith(modifiersContext, current.modifier_effect, '')
             else:
                 resultado_total_composto = resultados_compostos
                 arithContext = arithContext[:modifiersContextMarker['start']] + modifiersContext + arithContext[modifiersContextMarker['end']:]
@@ -54,39 +51,35 @@ while matchedArithContext:
                 # print(arithContext)
                 break
 
-        matched_d = re.search(rx.dice, arithContext)
+        matched_d = re.search(regex.DICE, arithContext)
 
+    current = Arithmetic()
     while 1:
-        op_is_mul = re.search(rx.arithMul, arithContext)
-        op_is_div = re.search(rx.arithDiv, arithContext)
-        op_is_add = re.search(rx.arithAdd, arithContext)
-        op_is_sub = re.search(rx.arithSub, arithContext)
-        
-        if op_is_mul:
-            mulResult = float(op_is_mul[1]) * float(op_is_mul[2])
-            arithContext = compute_arith(arithContext, op_is_mul, mulResult)
-        elif op_is_div:
-            divResult = float(op_is_div[1]) / float(op_is_div[2])
-            arithContext = compute_arith(arithContext, op_is_div, divResult)
-        elif op_is_add:
-            addResult = float(op_is_add[1]) + float(op_is_add[2])
-            arithContext = compute_arith(arithContext, op_is_add, addResult)
-        elif op_is_sub:
-            subResult = float(op_is_sub[1]) - float(op_is_sub[2])
-            arithContext = compute_arith(arithContext, op_is_sub, subResult)
+        if current.operator_is_mul(arithContext):
+            mulResult = current.factors[0] * current.factors[1]
+            arithContext = compute_arith(arithContext, current.operation, mulResult)
+        elif current.operator_is_div(arithContext):
+            divResult = current.factors[0] / current.factors[1]
+            arithContext = compute_arith(arithContext, current.operation, divResult)
+        elif current.operator_is_add(arithContext):
+            addResult = current.factors[0] + current.factors[1]
+            arithContext = compute_arith(arithContext, current.operation, addResult)
+        elif current.operator_is_sub(arithContext):
+            subResult = current.factors[0] - current.factors[1]
+            arithContext = compute_arith(arithContext, current.operation, subResult)
         else:
             # print(arithContext)
-            expressionRaw = expressionRaw[:matchedArithContext.start()] + arithContext + expressionRaw[matchedArithContext.end():]
+            expressionRaw = compute_arith(expressionRaw, matchedArithContext, arithContext)
             break
     
-    matchedArithContext = re.search(rx.arithContext, expressionRaw)
-    # print(expressionRaw)
+    matchedArithContext = re.search(regex.ARITH_CONTEXT, expressionRaw)
+    print(expressionRaw)
 
-expressionOutput = re.sub(rx.modifier, '', expressionInput).replace('.', ',')
-foundDices = re.findall(rx.diceOnly, expressionInput)
+expressionOutput = re.sub(regex.MODIFIER, '', expressionInput).replace('.', ',')
+foundDices = re.findall(regex.DICE_ONLY, expressionInput)
 for d in range(len(foundDices)):
-    expressionOutput = re.sub(rx.diceOnly, 'dice('+str(d+1)+')', expressionOutput, 1)
-foundOperators = list(set(re.findall(rx.operator, expressionOutput)))
+    expressionOutput = re.sub(regex.DICE_ONLY, 'dice('+str(d+1)+')', expressionOutput, 1)
+foundOperators = list(set(re.findall(regex.OPERATOR, expressionOutput)))
 for op in foundOperators:
     expressionOutput = expressionOutput.replace(op, ' '+op+' ')
 print(expressionOutput)
--- spike/spike_modules.py
+++ spike/spike_modules.py
@@ -1,22 +1,77 @@
-class DefaultRegexes:
-    operator = r'([\*\/\+\-])'
-    floatValue = r'(\-?\d+(?:\.\d+)?)'
-    arithContext = r'(?:\()([^()]+)(?:\))'
-
-    arithMul = floatValue + r'\*' + floatValue
-    arithDiv = floatValue + r'\/' + floatValue
-    arithAdd = floatValue + r'\+' + floatValue
-    arithSub = floatValue + r'\-' + floatValue
-
-    diceOnly = r'(\d+[Dd]\d+)'
-    dice = operator + r'?(\d+d\d+)'
-    modifier = operator + r'(\d+(?:\.\d+)?)(?i:(each|e))'
-    modifiersContext = r'(?:(?![\*\/\+\-]\-?\d+d).)*'
-
-    modifierMul = r'\*'+ floatValue +r'e'
-    modifierDiv = r'\/'+ floatValue +r'e'
-    modifierAdd = r'\+'+ floatValue +r'e'
-    modifierSub = r'\-'+ floatValue +r'e'
+import re
 
 def rolardados():
     return 1
+
+class DefaultRegexes:
+    OPERATOR = r'([\*\/\+\-])'
+    FLOAT_VALUE = r'(\-?\d+(?:\.\d+)?)'
+    ARITH_CONTEXT = r'(?:\()([^()]+)(?:\))'
+
+    ARITH_MUL = FLOAT_VALUE + r'\*' + FLOAT_VALUE
+    ARITH_DIV = FLOAT_VALUE + r'\/' + FLOAT_VALUE
+    ARITH_ADD = FLOAT_VALUE + r'\+' + FLOAT_VALUE
+    ARITH_SUB = FLOAT_VALUE + r'\-' + FLOAT_VALUE
+
+    DICE_ONLY = r'(\d+[Dd]\d+)'
+    DICE = OPERATOR + r'?(\d+d\d+)'
+    MODIFIER = OPERATOR + r'(\d+(?:\.\d+)?)(?i:(each|e))'
+    MODIFIERS_CONTEXT = r'(?:(?![\*\/\+\-]\-?\d+d).)*'
+
+    MODIFIER_MUL = r'\*'+ FLOAT_VALUE +r'e'
+    MODIFIER_DIV = r'\/'+ FLOAT_VALUE +r'e'
+    MODIFIER_ADD = r'\+'+ FLOAT_VALUE +r'e'
+    MODIFIER_SUB = r'\-'+ FLOAT_VALUE +r'e'
+
+class Arithmetic (DefaultRegexes):
+    def operator_is_mul (self, context):
+        self.operation = re.search(self.ARITH_MUL, context)
+        self.__set_operation_factors()
+        return self.operation
+    
+    def operator_is_div (self, context):
+        self.operation = re.search(self.ARITH_DIV, context)
+        self.__set_operation_factors()
+        return self.operation
+    
+    def operator_is_add (self, context):
+        self.operation = re.search(self.ARITH_ADD, context)
+        self.__set_operation_factors()
+        return self.operation
+    
+    def operator_is_sub (self, context):
+        self.operation = re.search(self.ARITH_SUB, context)
+        self.__set_operation_factors()
+        return self.operation
+
+    def __set_operation_factors (self):
+        if self.operation:
+            self.factors = [
+                float(self.operation[1]),
+                float(self.operation[2])
+            ]
+
+class DiceEffect (DefaultRegexes):
+    def modifier_operator_is_mul (self, context):
+        self.modifier_effect = re.search(self.MODIFIER_MUL, context)
+        self.__set_modifier_value()
+        return self.modifier_effect
+
+    def modifier_operator_is_div (self, context):
+        self.modifier_effect = re.search(self.MODIFIER_DIV, context)
+        self.__set_modifier_value()
+        return self.modifier_effect
+    
+    def modifier_operator_is_add (self, context):
+        self.modifier_effect = re.search(self.MODIFIER_ADD, context)
+        self.__set_modifier_value()
+        return self.modifier_effect
+    
+    def modifier_operator_is_sub (self, context):
+        self.modifier_effect = re.search(self.MODIFIER_SUB, context)
+        self.__set_modifier_value()
+        return self.modifier_effect
+
+    def __set_modifier_value (self):
+        if self.modifier_effect:
+            self.modifier_value = float(self.modifier_effect[1])
