1576c9d4854a7459e6de59476a5d2cd9f09df86f
ngsylar <ngsylar@gmail.com>
2021-06-25 11:08:27 -0300

    Stage for another cleaning

---
+++ spike/spike_arithmetic.py
@@ -0,0 +1,93 @@
+import re
+from spike_regexes import DefaultRegexes
+
+class Operation (DefaultRegexes):
+    def __init__ (self):
+        self.operation_is_not_performed = True
+
+    def operator_is_mul (self, expression):
+        self.operation_match = re.search(self.ARITH_MUL, expression)
+        self.__set_operation_factors()
+        return self.operation_match
+    
+    def operator_is_div (self, expression):
+        self.operation_match = re.search(self.ARITH_DIV, expression)
+        self.__set_operation_factors()
+        return self.operation_match
+    
+    def operator_is_add (self, expression):
+        self.operation_match = re.search(self.ARITH_ADD, expression)
+        self.__set_operation_factors()
+        return self.operation_match
+    
+    def operator_is_sub (self, expression):
+        self.operation_match = re.search(self.ARITH_SUB, expression)
+        self.__set_operation_factors()
+        return self.operation_match
+
+    def __set_operation_factors (self):
+        if self.operation_match:
+            self.factors = [
+                float(self.operation_match[1]),
+                float(self.operation_match[2])]
+
+
+class Modifier (DefaultRegexes):
+    def __init__ (self):
+        self.modifier_is_not_applied = True
+
+    def modifier_operator_is_mul (self, expression_raw):
+        self.modifier_match = re.search(self.MODIFIER_MUL, expression_raw)
+        self.__set_modifier_value()
+        return self.modifier_match
+
+    def modifier_operator_is_div (self, expression_raw):
+        self.modifier_match = re.search(self.MODIFIER_DIV, expression_raw)
+        self.__set_modifier_value()
+        return self.modifier_match
+    
+    def modifier_operator_is_add (self, expression_raw):
+        self.modifier_match = re.search(self.MODIFIER_ADD, expression_raw)
+        self.__set_modifier_value()
+        return self.modifier_match
+    
+    def modifier_operator_is_sub (self, expression_raw):
+        self.modifier_match = re.search(self.MODIFIER_SUB, expression_raw)
+        self.__set_modifier_value()
+        return self.modifier_match
+
+    def __set_modifier_value (self):
+        if self.modifier_match:
+            self.modifier_value = float(self.modifier_match[1])
+
+
+class Expression (DefaultRegexes):
+    def __init__ (self, expression, pattern_expression=False):
+        if pattern_expression:
+            self.expression = '('+expression.lower().replace('each', 'e').replace(',', '.')+')'
+        else:
+            self.expression = expression
+    
+    def has_inner_expression (self):
+        self.inner_expression_match = re.search(self.ARITH_EXPRESSION, self.expression)
+        if self.inner_expression_match:
+            self.inner_expression_raw = self.inner_expression_match[1]
+        return self.inner_expression_match
+    
+    def has_dice (self):
+        self.inner_dice_match = re.search(self.DICE, self.expression)
+        if self.inner_dice_match:
+            position_adjust = self.inner_dice_match.end()
+            dice_modifiers_match = re.match(self.MODIFIERS_RAW, self.expression[position_adjust:])
+            self.dice_modifiers_raw = dice_modifiers_match[0]
+            self.dice_modifiers_position = {
+                'start': dice_modifiers_match.start() + position_adjust,
+                'end': dice_modifiers_match.end() + position_adjust}
+        return self.inner_dice_match
+
+    def replace (self, operation, result):
+        if type(operation) == re.Match:
+            self.expression = self.expression[:operation.start()] + str(result) + self.expression[operation.end():]
+        elif type(operation) == dict:
+            self.expression = self.expression[:operation['start']] + str(result) + self.expression[operation['end']:]
+        return self.expression
--- spike/spike_arithmetic_dice.py
+++ spike/spike_arithmetic_dice.py
@@ -1,5 +1,6 @@
 import re
-from spike_modules import DefaultRegexes as regex, RollExpression, ArithmeticOperation, DiceModifier
+import spike_arithmetic
+from spike_regexes import DefaultRegexes as regex
 
 userInput = '(((-1D8+4/2+100*3,22+2D20-1each+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
 #expressionInput = '(((4/2+100*3,22+2D20-1e+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+2)/2'
@@ -8,59 +9,59 @@ userInput = '(((-1D8+4/2+100*3,22+2D20-1each+1*2e-2+1d8)*2+(1+3)/(2+2-(1+1))+4)+
 def rolardados():
     return 1
 
-expression = RollExpression(userInput, pattern_expression=True)
-print(expression.raw)
+expression = spike_arithmetic.Expression(userInput, pattern_expression=True)
+print(expression.expression)
 
 while expression.has_inner_expression():
-    innerExpression = RollExpression(expression.inner_expression_raw)
+    innerExpression = spike_arithmetic.Expression(expression.inner_expression_raw)
 
     while innerExpression.has_dice():
-        modifiers = RollExpression(innerExpression.dice_modifiers_raw)
+        modifiers = spike_arithmetic.Expression(innerExpression.dice_modifiers_raw)
 
         # editar: aqui usar o lancamento de dado (neste trecho de codigo foi usado apenas uma substituicao simples, pois o lancamento ja foi implementado, nao sendo o foco deste teste)
         resultados = []
         resultados_simples = rolardados()
         resultados_compostos = resultados_simples
         
-        current = DiceModifier()
+        current = spike_arithmetic.Modifier()
         while current.modifier_is_not_applied:
-            if current.modifier_operator_is_mul(modifiers.raw):
+            if current.modifier_operator_is_mul(modifiers.expression):
                 resultados_compostos *= current.modifier_value
-                modifiers.replace(current.modifier, '')
-            elif current.modifier_operator_is_div(modifiers.raw):
+                modifiers.replace(current.modifier_match, '')
+            elif current.modifier_operator_is_div(modifiers.expression):
                 resultados_compostos /= current.modifier_value
-                modifiers.replace(current.modifier, '')
-            elif current.modifier_operator_is_add(modifiers.raw):
+                modifiers.replace(current.modifier_match, '')
+            elif current.modifier_operator_is_add(modifiers.expression):
                 resultados_compostos += current.modifier_value
-                modifiers.replace(current.modifier, '')
-            elif current.modifier_operator_is_sub(modifiers.raw):
+                modifiers.replace(current.modifier_match, '')
+            elif current.modifier_operator_is_sub(modifiers.expression):
                 resultados_compostos -= current.modifier_value
-                modifiers.replace(current.modifier, '')
+                modifiers.replace(current.modifier_match, '')
             else:
                 resultado_total_composto = resultados_compostos
-                innerExpression.replace(innerExpression.dice_modifiers_position, modifiers.raw)
+                innerExpression.replace(innerExpression.dice_modifiers_position, modifiers.expression)
                 innerExpression.replace(innerExpression.inner_dice_match, resultado_total_composto)
                 break
 
-    current = ArithmeticOperation()
+    current = spike_arithmetic.Operation()
     while current.operation_is_not_performed:
-        if current.operator_is_mul(innerExpression.raw):
+        if current.operator_is_mul(innerExpression.expression):
             opResult = current.factors[0] * current.factors[1]
-            innerExpression.replace(current.operation, opResult)
-        elif current.operator_is_div(innerExpression.raw):
+            innerExpression.replace(current.operation_match, opResult)
+        elif current.operator_is_div(innerExpression.expression):
             opResult = current.factors[0] / current.factors[1]
-            innerExpression.replace(current.operation, opResult)
-        elif current.operator_is_add(innerExpression.raw):
+            innerExpression.replace(current.operation_match, opResult)
+        elif current.operator_is_add(innerExpression.expression):
             opResult = current.factors[0] + current.factors[1]
-            innerExpression.replace(current.operation, opResult)
-        elif current.operator_is_sub(innerExpression.raw):
+            innerExpression.replace(current.operation_match, opResult)
+        elif current.operator_is_sub(innerExpression.expression):
             opResult = current.factors[0] - current.factors[1]
-            innerExpression.replace(current.operation, opResult)
+            innerExpression.replace(current.operation_match, opResult)
         else:
-            expression.replace(expression.inner_expression_match, innerExpression.raw)
+            expression.replace(expression.inner_expression_match, innerExpression.expression)
             break
     
-    print(expression.raw)
+    print(expression.expression)
 
 expressionOutput = re.sub(regex.MODIFIER, '', userInput).replace('.', ',')
 foundDices = re.findall(regex.DICE, userInput)
