e4563d37c909ad00aa0bdf926198b33ba024bf62
ngsylar <ngsylar@gmail.com>
2021-07-05 15:40:41 -0300

    Help update

--- src/helpguide.py
+++ src/helpguide.py
@@ -67,23 +67,27 @@ class UserGuide:
       ':game_die: **Gambling**\n',
       '_To perform a game action, roll the dice._\n'
 
-      '\nBasic structure of a game die - where _r_ is the range on the die and _a_ is the amount of rolls for this die:',
-      txtop.BBOX + 'ini\n[a]d[r]' + txtop.BBOX,
-      txtop.BBOX + '2d20 for example will roll a twenty-sided die twice' + txtop.BBOX,
+      '\nBasic structure of a game die - where _r_ is the range on the die and _a_ is the amount of this die to be rolled:',
+      txtop.BBOX + 'css\n[a]d[r]' + txtop.BBOX,
+      txtop.BBOX + '2d20 for example will roll two twenty-sided die' + txtop.BBOX,
 
       '\nYou can add modifiers to each result of a die immediately after it - where _op_ is a mathematical operator whose priority is always the order in which they appear and _v_ is the value to modify the die results:',
-      txtop.BBOX + 'ini\n[op][v]e' + txtop.BBOX,
-      txtop.BBOX + '2d20+2e for example will add 2 to the value of each result of a die' + txtop.BBOX,
+      txtop.BBOX + 'css\n[op][v]e' + txtop.BBOX,
+      txtop.BBOX + '2d20+2e for example will add 2 to the value of each result of this die' + txtop.BBOX,
 
-      '\nYou can even input multiple game dice into a single arithmetic expression. Take for example this valid input:',
-      txtop.BBOX + 'arm\n2d4+2-(2d10+2e)' + txtop.BBOX,
+      '\nYou can repeat the roll of the same die multiple times and the results will be shown individually for each repetition. See the notation below - where _rep_ is the number of repetitions of the rolls:',
+      txtop.BBOX + 'css\n[rep]#[dice][modifiers]' + txtop.BBOX,
+      txtop.BBOX + '6#4d6-2e for example will roll four six-sided dice six times and apply the -2 modifier to every result of this die' + txtop.BBOX,
+
+      '\nYou can even input several game dice into a single arithmetic expression - that\'s the definition of game action. Take for example this valid input:',
+      txtop.BBOX + 'arm\n2#2d10+2e-(2d4+2e+2) for the glory' + txtop.BBOX,
       
       '\nIf you want to take a hidden action or want its results to be secret, use a flag - indicated by _f_ - at the beginning of the input, as shown below:',
-      txtop.BBOX + 'ini\n[f][expression] [optional message]' + txtop.BBOX,
+      txtop.BBOX + 'css\n[f][expression] [optional message]' + txtop.BBOX,
       txtop.BBOX + 'asciidoc\n',
-      '            :: empty to reveal the action results\n',
-      's           :: to hide the action results\n',
-      'h           :: to perform a hidden action' + txtop.BBOX,
+      '     :: empty to reveal the action results\n',
+      's    :: to hide the action results\n',
+      'h    :: to perform a hidden action' + txtop.BBOX,
       txtop.BBOX + 's1d20+2 for example hides the action results and this is an optional message' + txtop.BBOX]
     
     embedBox.description=''.join(embedBox_text)
